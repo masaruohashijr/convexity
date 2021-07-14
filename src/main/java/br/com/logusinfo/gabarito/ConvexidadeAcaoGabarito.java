efe4075e94ca2bb251f94e629b40efd136e301a5
Masaru Ohashi Jr <masaru@vindixit.com>
2021-07-14 12:06:30 -0300

    novas instrucoes

--- src/main/java/br/com/logusinfo/gabarito/ConvexidadeAcaoGabarito.java
+++ src/main/java/br/com/logusinfo/gabarito/ConvexidadeAcaoGabarito.java
@@ -1,11 +1,16 @@
 package br.com.logusinfo.gabarito;
 
-import java.util.LinkedList;
-import java.util.List;
-/*
- * Tendo um array de números que representa a volatilidade do preço de uma ação.
- * Considere a convexidade como sendo uma curva crescente de valorização do preço da ação.
- * Desenvolva um código que identifica se a curva gerada pelos preços é convexa ou não.   	 
+/* Tendo um array de números que representa a volatilidade do preço de uma ação.
+ * R$ 6, R$ 4, R$ 2, R$ 8 são os preços de aberturas de uma ação VOCE11 
+ * na Bolsa de Valores de Brasília (fictício).
+ * Uma sequência ser Convexa significa que os preços estão em Alta, e 
+ * uma sequência ser Côncava que dizer que os preços estão em Baixa.  	 
+ * Portanto, considere a convexidade como sendo uma curva crescente de valorização do preço da ação.
+ * Desenvolva um código que identifica se a curva gerada pelos preços é convexa ou não.
+ * Exemplo: A sequência 6 > 4 > 2 < 8 é convexa.
+ * Porque 4 - 6 = -2,  2 - 4 = -2, 8 - 2 = 6, assim como -2 + (-2) + 6 dá 2 positivo, 
+ * conclui-se que a sequência de preços dessa AÇÃO é convexa, 
+ * pois os acréscimos superam os decréscimos nesse período da análise.
  */
 public class ConvexidadeAcaoGabarito {
 	public boolean isConvexo(double[] cotacoes) {
